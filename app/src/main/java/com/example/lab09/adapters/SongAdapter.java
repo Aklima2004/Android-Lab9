41bee46efebc00a99f852a983c1ba3de3ecf03bb
Aklima2004 <iskakbaeva.aklima@mail.ru>
2025-04-28 01:59:05 +0500

    adding SongAdapter and item layout for song list

--- app/src/main/java/com/example/lab09/adapters/SongAdapter.java
+++ app/src/main/java/com/example/lab09/adapters/SongAdapter.java
@@ -1,4 +1,52 @@
 package com.example.lab09.adapters;
 
-public class SongAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lab09.R;
+import com.example.lab09.model.Song;
+
+import java.util.List;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder> {
+
+    private List<Song> songList;
+
+    public SongAdapter(List<Song> songList) {
+        this.songList = songList;
+    }
+
+    @NonNull
+    @Override
+    public SongViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_song, parent, false);
+        return new SongViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SongViewHolder holder, int position) {
+        Song song = songList.get(position);
+        holder.textViewTitle.setText(song.getTitle());
+        holder.textViewArtist.setText(song.getArtist());
+    }
+
+    @Override
+    public int getItemCount() {
+        return songList.size();
+    }
+
+    static class SongViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewTitle, textViewArtist;
+
+        public SongViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewArtist = itemView.findViewById(R.id.textViewArtist);
+        }
+    }
 }
