f83b024de50f93e9a2412cc07031cfb886c9da2c
Aklima2004 <iskakbaeva.aklima@mail.ru>
2025-04-28 03:12:40 +0500

    adding HomeActivity with BottomNavigationView

--- app/src/main/java/com/example/lab09/activities/HomeActivity.java
+++ app/src/main/java/com/example/lab09/activities/HomeActivity.java
@@ -1,48 +1,38 @@
 package com.example.lab09.activities;
 
-import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.content.SharedPreferences;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import com.example.lab09.R;
-import com.example.lab09.adapters.SongAdapter;
-import com.example.lab09.model.Song;
-import com.example.lab09.utils.FileHelper;
 
-import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.lab09.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private SongAdapter songAdapter;
-    private List<Song> songList;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        recyclerView = findViewById(R.id.recyclerViewSongs);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-        // Загрузка песен из локального файла
-        songList = FileHelper.readSongs(this);
-        songAdapter = new SongAdapter(songList);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(songAdapter);
-    }
-
-    @Override
-    public void onBackPressed() {
-        new AlertDialog.Builder(this)
-                .setTitle("Exit")
-                .setMessage("Are you sure you want to exit?")
-                .setPositiveButton("Yes", (dialog, which) -> finish())
-                .setNegativeButton("No", null)
-                .show();
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    // Уже на главной, ничего не делаем
+                    return true;
+                case R.id.nav_mymusic:
+                    startActivity(new Intent(HomeActivity.this, MyMusicActivity.class));
+                    return true;
+                case R.id.nav_profile:
+                    startActivity(new Intent(HomeActivity.this, ProfileActivity.class));
+                    return true;
+            }
+            return false;
+        });
     }
 }
